229652db96eaa31633f77c28cc98bb10d73728f7
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2019-04-06 15:38:35 +0200

    Updates

---
+++ precache-manifest.f2f28c0716eff81f922ec61e91818dce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28fbb7debeb67f585d77",
+    "url": "/snake/static/js/runtime~main.ccc81976.js"
+  },
+  {
+    "revision": "d4f38f69907121d5eabd",
+    "url": "/snake/static/js/main.a64327a8.chunk.js"
+  },
+  {
+    "revision": "7a7671dc4c0acba03378",
+    "url": "/snake/static/js/2.eb8bd00b.chunk.js"
+  },
+  {
+    "revision": "d4f38f69907121d5eabd",
+    "url": "/snake/static/css/main.751bb80a.chunk.css"
+  },
+  {
+    "revision": "43bc4ac1e1bebac835184036f29984ee",
+    "url": "/snake/index.html"
+  }
+];
